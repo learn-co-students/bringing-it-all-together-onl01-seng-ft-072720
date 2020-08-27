02bc95ce026eb0a3a840f02a532fc81b1c8ace45
RyanBegnaud <Ryanbegnaud13@gmail.com>
2020-08-27 16:11:05 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,93 @@
+class Dog
+    attr_accessor :name, :breed, :id
+
+    def initialize(name:, breed:, id: nil)
+        @name = name 
+        @breed = breed 
+        @id = id
+    end
+
+    def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT,
+        breed TEXT
+    )   
+SQL
+    DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL 
+        DROP TABLE dogs
+        SQL
+    DB[:conn].execute(sql)
+    end
+
+    def save 
+        sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+        SQL
+    
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0] 
+    self
+    end
+
+    def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+        dog
+    end
+
+    def self.new_from_db(row)
+        dog = Dog.new(name: row[1], breed: row[2], id: row[0]) 
+    end
+
+    def self.find_by_id(id)
+        sql = <<-SQL 
+        SELECT * FROM dogs 
+        WHERE id = ?
+        SQL
+
+        result = DB[:conn].execute(sql, id)[0]
+        self.new_from_db(result)
+    end
+
+   def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
+
+
+      dog = DB[:conn].execute(sql, name, breed)[0]
+
+      if dog
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs 
+    WHERE name = ?
+    SQL
+
+    result = DB[:conn].execute(sql,name)[0]
+    self.new_from_db(result)
+  end
+
+  def update 
+    sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+end
